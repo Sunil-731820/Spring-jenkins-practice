217080a5e86ab6cba17638661751defdbfe695f1
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2023-09-17 21:49:53 +0530

    Second Commit

--- src/main/java/com/java/jenkins/SpringJenkinsPracticeApplication.java
+++ src/main/java/com/java/jenkins/SpringJenkinsPracticeApplication.java
@@ -1,11 +1,15 @@
 package com.java.jenkins;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.classic.Logger;
 
 @SpringBootApplication
 public class SpringJenkinsPracticeApplication {
 
+	Logger logger = LoggerFactory.getILoggerFactory();
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJenkinsPracticeApplication.class, args);
 	}
